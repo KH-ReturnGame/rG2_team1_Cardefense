116679d8eb20d9312bb301e836fb061a40fe8227
necopilot-16 <mwj4055@gmail.com>
2025-09-12 13:39:40 +0900

    에러 해결+재생성

--- Assets/Scritps/Bullet.cs
+++ Assets/Scritps/Bullet.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject enemy;    // 목표
+    public float speed = 5f;    // 속도
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (enemy == null)
+        {
+            Destroy(gameObject); // 목표가 없으면 삭제
+            return;
+        }
+
+        // 목표 방향으로 이동
+        Vector3 dir = (enemy.transform.position - transform.position).normalized;
+        transform.position += dir * speed * Time.deltaTime;
     }
 }
+
