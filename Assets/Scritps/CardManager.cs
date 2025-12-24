530e0899e8061d404e22e421f9c7cad5a854a339
necopilot-16 <mwj4055@gmail.com>
2025-12-24 16:57:05 +0900

    완성 - 튜토리얼 제외

--- Assets/Scritps/CardManager.cs
+++ Assets/Scritps/CardManager.cs
@@ -2,14 +2,34 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public GameObject cardprefab;
-    public int cardCount = 3;
+    public GameObject cardPrefab;
+    public CardBoard cardBoard;
 
+    public int maxCardCount = 5;
+    public int cardCount = 3; // 테스트용
+
+    // 기존 카드 생성 (그대로 둠)
     public void Spawncards()
     {
         for (int i = 0; i < cardCount; i++)
         {
-            Instantiate(cardprefab);
+            GameObject card = Instantiate(cardPrefab);
+
+            if (!cardBoard.TryAddCard(card))
+            {
+                Destroy(card);
+                return;
+            }
         }
     }
+
+    //  이번 단계: 카드 개수 확인용
+    public void DebugCheckCardCount()
+    {
+        int current = cardBoard.GetCurrentCardCount();
+        int need = maxCardCount - current;
+
+        Debug.Log($"현재 카드 수: {current}, 필요한 카드 수: {need}");
+    }
 }
+
