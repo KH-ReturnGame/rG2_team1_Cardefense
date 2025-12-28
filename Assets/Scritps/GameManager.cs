02561346157e204c733317fb33d1c109db130ea7
necopilot-16 <mwj4055@gmail.com>
2025-12-29 03:34:00 +0900

    끝이 보입니다

--- Assets/Scritps/GameManager.cs
+++ Assets/Scritps/GameManager.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;  // 싱글톤
-    public int playerHP = 20;            // 플레이어 체력
+    public static GameManager Instance;
+
+    public int playerHP = 20;
+
+    [Header("Game Over UI")]
+    public GameObject gameOverPanel;
+    public TMP_Text waveText;   // TMP 쓰면 TMP_Text로 바꾸면 됨
 
     void Awake()
     {
-        Instance = this; // 실행되면 Instance 채워짐
+        Instance = this;
+        Time.timeScale = 1f; // 혹시 이전에 멈췄을 경우 대비
     }
 
     public void PlayerTakeDamage(int dmg)
@@ -17,7 +26,24 @@ public class GameManager : MonoBehaviour
 
         if (playerHP <= 0)
         {
-            Debug.Log("Game Over!");
+            GameOver();
         }
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0f; // 게임 멈춤
+
+        gameOverPanel.SetActive(true);
+
+        int wave = TurnManager.Instance.currentTurn;
+        waveText.text = $"도달 웨이브 : {wave}";
+    }
+
+    // 버튼에 연결할 함수
+    public void GoToStartScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SelectScene"); // 네 시작 씬 이름
     }
 }
